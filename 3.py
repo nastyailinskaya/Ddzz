c3d71e86959802247fa72c219bec5183c8206c8a
nastyailinskaya <114681407+nastyailinskaya@users.noreply.github.com>
2022-10-13 20:17:17 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,7 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> r = int(input("Введите радиус"))
+Введите радиус40
+>>> print("S=", 3.14159265359*r**2)
+S= 5026.548245744
+>>> 
